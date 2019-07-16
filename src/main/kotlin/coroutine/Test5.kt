e108f8d2cb90d13d06bd27749c48e23588b4c380
talent_mbp2018 <hl010738@126.com>
2019-07-16 17:14:48 +0800

    协程 job

---
+++ src/main/kotlin/coroutine/Test5.kt
@@ -0,0 +1,19 @@
+package coroutine
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking{
+    val job = GlobalScope.launch {
+        delay(1000)
+        println("Kotlin coroutine")
+    }
+
+    println("---------------")
+
+    job.join()
+
+    println("================")
+}
